39f585bc3bd8d125a32f890e10ca84dbeaabd664
Wissavat <112836263+Wissavat@users.noreply.github.com>
2023-04-03 15:07:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// this function clear all the values
+function clearScreen(){
+    document.getElementById("result").value = " ";
+}
+// this function display values
+function display(value){
+    document.getElementById("result").value += value;
+}
+// This function evaluates the expression and return result
+function calculate(){
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value= q;
+}
